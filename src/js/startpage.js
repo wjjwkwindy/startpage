b8921924f53ed37a48bf547d5a95e327bdeed99a
wjjwkwindy <wjjwkwindy@gmail.com>
2022-11-03 14:56:45 +0800

    feat: 添加分类功能；添加书签；优化样式；

--- src/js/startpage.js
+++ src/js/startpage.js
@@ -1,26 +1,69 @@
-$.loadJSON('config.json', (configJSON) => {
-    let linkCount = 0,
-        cateCount = 0,
-        urlReg = /http(s?):\/\/(.+)\//;
+let configJSON = {};
 
-    let linkCard = document.querySelector('#test>div');
+$.loadJSON('config.json', (reponse) => {
+    configJSON = reponse;
+    forCate();
+});
+
+// 循环链接
+function forLlinks(cate) {
+    let linkCard = document.querySelector('#card-container>div');
+    let urlReg = /http(s?):\/\/(.+[^\/])/;
+    let linkCount = 0;
+
+    document.querySelector('#card-container').innerHTML = '';
+
+    for (const linkCate of configJSON.stock) {
+        for (const link of linkCate.links) {
+            linkCount++;
+
+            if (linkCate.cate === cate) {
+                let linkCardTemp = linkCard.cloneNode(true);
+                linkCardTemp.querySelector('h2').textContent = link.name;
+                linkCardTemp.querySelector('a').href = link.url;
+                linkCardTemp.querySelector('span').textContent = urlReg.exec(link.url)[2]; // 匹配网址
+                document.querySelector('#card-container').appendChild(linkCardTemp);
+            }
+        }
+    }
+    linkCard.style.display = 'none';
+
+    document.querySelector('#linkCount').textContent = linkCount;
+}
+
+// 循环分类
+function forCate() {
+    let cateCard = document.querySelector('#cate-container>button');
+    let cateCount = 0;
 
     for (const linkCate of configJSON.stock) {
         cateCount++;
-        for (const link of linkCate.links) {
-            linkCount++;
-            let linkCardTemp = linkCard.cloneNode(true);
-            linkCardTemp.querySelector('h2').textContent = link.name;
-            linkCardTemp.querySelector('a').href = link.url;
-            linkCardTemp.querySelector('span').textContent = urlReg.exec(link.url)[2]; // 匹配网址
-            document.querySelector('#test').appendChild(linkCardTemp);
+        let cateCardTemp = cateCard.cloneNode(true);
+
+        cateCardTemp.querySelector('span').textContent = linkCate.cate;
+        cateCardTemp.querySelector('svg').outerHTML = linkCate.icon;
+        cateCardTemp.dataset.catename = linkCate.cate;
+
+        if (cateCount === 1) {
+            forLlinks(linkCate.cate);
+        } else {
+            cateCardTemp.classList.remove('border-slate-600');
         }
+
+        cateCardTemp.addEventListener('click', function () {
+            forLlinks(this.dataset.catename);
+            Array.from(this.parentElement.children).forEach(el => {
+                el.classList.remove('border-slate-600');
+            })
+            this.classList.add('border-slate-600');
+        })
+
+        document.querySelector('#cate-container').appendChild(cateCardTemp);
     }
 
-    linkCard.style.display = 'none';
-    document.querySelector('#linkCount').textContent = linkCount;
+    cateCard.style.display = 'none';
     document.querySelector('#cateCount').textContent = cateCount;
-});
+}
 
 function uptime() {
     $.loadJSON('http://207.246.84.123:8090/https://status.arcticalpaca.com/json/stats.json', (data) => {
